b9f8968b49c6f8406db245ea583fb0e073a8ce30
faizhidayatulloh <faizulurrosyad433@gmail.com>
2024-08-01 12:54:52 +0700

    ubah

--- code.js
+++
@@ -1,9 +0,0 @@
-let kata;
-kata = window.prompt('Hai, siapa nama kamu?    \nKetika nama anda :');
-if ( kata == ""|| kata == null ) {
-    kata = 'tolong ketik nama anda, dan tekan ok !'
-    document.getElementById('faiz').innerHTML = kata;
-} else {
-    document.getElementById('faiz').innerHTML =`halo ${kata}, bagaimana kabar anda hari ini? kapan menikah? kapan punya anak?
-    kapan masuk surga ?`
-}
