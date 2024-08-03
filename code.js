8bbd73de2cfd4a5f6112371e5d5a01afb6b3b0ba
faizhidayatulloh <faizulurrosyad433@gmail.com>
2024-08-03 11:54:43 +0700

    ubh

--- code.js
+++ code.js
@@ -1,9 +1,20 @@
-function buka() {
-    let no = 81910288140;
-    let pesan = "Halo mas Faiz\nkapan anda menikah lagi?????"
-    let pesanUrl = encodeURIComponent(pesan)
-    let url = `https://wa.me/${no}?text=${encodeURIComponent(pesan)}`
-    window.open(url,'_blank')
+function showLocation (){
+    navigator.geolocation.getCurrentPosition(cari);
 }
+function cari(position) {
+    let x = document.getElementById('faiz');
+    let lintang = position.coords.latitude;
+    let bujur = position.coords.longitude;
+    x.innerHTML = `garis lintang : ${lintang}<br/>
+    garis bujur : ${bujur}`
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${lintang}&lon=${bujur}&format=json`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    const address = data.display_name;
+    document.getElementById('alin').innerHTML = `alamat : ${address}`
+  })
+  .catch(error => console.error('Error:', error));
 
-document.getElementById('wa').addEventListener('click',buka)
+}
+document.getElementById('btn').addEventListener('click',showLocation)
