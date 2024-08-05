5603f2c41a02967f15c58a254abbd70cd18d2787
faizhidayatulloh <faizulurrosyad433@gmail.com>
2024-08-05 08:49:21 +0700

    ubah

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
+//menulis di layar
 function tulis(value) {
     document.getElementById('layar').value+=value;
+}
+//bersihkan layar
+function bersihkan() {
+    document.getElementById('layar').value ="";
 }
